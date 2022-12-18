30a342b326106d41bac5ff712742d8da5ede7bfd
littletrainee <arechery.a1111@gmail.com>
2022-12-18 21:49:36 +0800

    Fix Only Integer

--- StringToFloat.go
+++ StringToFloat.go
@@ -26,21 +26,30 @@ func GetFloat64(s string) float64 {
 	// check has nagtive
 	s, Negative = checknagtive(s)
 
-	// Split Integer and Float Part
-	IntAndFloat = strings.Split(s, ".")
+	// check has conatins float
+	if strings.Contains(s, ".") {
+		// Split Integer and Float Part
+		IntAndFloat = strings.Split(s, ".")
 
-	// convert string to integer
-	Integer, err = strconv.Atoi(IntAndFloat[0])
-	if err != nil {
-		log.Println(err)
-	}
-	Float, err = strconv.Atoi(IntAndFloat[1])
-	if err != nil {
-		log.Println(err)
-	}
+		// convert string to integer
+		Integer, err = strconv.Atoi(IntAndFloat[0])
+		if err != nil {
+			log.Println(err)
+		}
+		Float, err = strconv.Atoi(IntAndFloat[1])
+		if err != nil {
+			log.Println(err)
+		}
 
-	// add Integer and Float Part
-	Result = float64(Integer) + float64(Float)/math.Pow(10, float64(len(IntAndFloat[1])))
+		// add Integer and Float Part
+		Result = float64(Integer) + float64(Float)/math.Pow(10, float64(len(IntAndFloat[1])))
+	} else {
+		Integer, err = strconv.Atoi((s))
+		if err != nil {
+			log.Println(err)
+		}
+		Result = float64(Integer)
+	}
 
 	// check orgin is nagtive
 	if Negative {
